adf5eeef97d847cb18a8537281ed88c59c1f34df
parkjunsoo <01junsoo@gmail.com>
2024-05-11 22:33:40 +0900

    feat: init eslint

--- src/core/api.ts
+++ src/core/api.ts
@@ -1,7 +1,29 @@
 import { api } from '../core/service';
 
-export const getAllCoinList = () => {
-  return api.service.get('/coins/markeks').then((response) => {
-    return response && response.data;
-  });
+interface GetAllCoinListParams {
+  vs_currency: string;
+  order?: string;
+  per_page?: number;
+  page?: number;
+  sparkline?: boolean;
+}
+
+export const getAllCoinList = ({
+  vs_currency,
+  order,
+  per_page,
+  page,
+  sparkline,
+}: GetAllCoinListParams) => {
+  return api.service
+    .get('/coins/markeks', {
+      vs_currency,
+      order,
+      per_page,
+      page,
+      sparkline,
+    })
+    .then((response) => {
+      return response && response.data;
+    });
 };
--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -1,19 +1,23 @@
-import React, { Children, useEffect, useState } from "react";
-import styled from "styled-components";
-import { Theme } from "@radix-ui/themes";
-import { Container } from "./MainLayout.style";
-import * as Tabs from "@radix-ui/react-tabs";
-import "@radix-ui/themes/styles.css";
-import { getAllCoinList } from "../core/api";
-import Header from "../components/Header";
-import { Outlet } from "react-router-dom";
+import React, { Children, useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { Theme } from '@radix-ui/themes';
+import { Container } from './MainLayout.style';
+import * as Tabs from '@radix-ui/react-tabs';
+import '@radix-ui/themes/styles.css';
+import { getAllCoinList } from '../core/api';
+import Header from '../components/Header';
+import { Outlet } from 'react-router-dom';
 interface MainLayoutProps {}
 
 const MainLayout: React.FC<MainLayoutProps> = ({}: MainLayoutProps) => {
-  // const [allList, setAllList] = useState([]);
-  // useEffect(() => {
-  //   getAllCoinList();
-  // }, []);
+  const [allList, setAllList] = useState([]);
+  useEffect(() => {
+    getAllCoinList({
+      vs_currency: 'usd',
+    }).then((res: any) => {
+      console.log(res);
+    });
+  }, []);
 
   return (
     <Container>
